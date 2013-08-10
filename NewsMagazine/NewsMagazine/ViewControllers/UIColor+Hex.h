4406a60fbe9cbdb07e70fb1fe160065f9fb0fb59
Michael henry Pantaleon <michaelhenry119@yahoo.com.ph>
2013-08-10 22:33:46 +0800

    added UIColor Category for HexString

---
+++ NewsMagazine/NewsMagazine/ViewControllers/UIColor+Hex.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Hex.h
+//  NewsMagazine
+//
+//  Created by Michael henry Pantaleon on 8/10/13.
+//  Copyright (c) 2013 Michael Henry Pantaleon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Hex)
++ (UIColor*)colorWithHexString:(NSString*)hex;
+@end
---
+++ NewsMagazine/NewsMagazine/ViewControllers/UIColor+Hex.m
@@ -0,0 +1,48 @@
+//
+//  UIColor+Hex.m
+//  NewsMagazine
+//
+//  Created by Michael henry Pantaleon on 8/10/13.
+//  Copyright (c) 2013 Michael Henry Pantaleon. All rights reserved.
+//
+
+#import "UIColor+Hex.h"
+
+@implementation UIColor (Hex)
+
++ (UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+@end
